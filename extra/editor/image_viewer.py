19ac65e8f5b017375fd1ba92efd8655e8e80a233
Poul Sander <poul@poulsander.com>
2020-04-05 10:34:01 +0200

    pylint

--- extra/editor/image_viewer.py
+++ extra/editor/image_viewer.py
@@ -8,27 +8,27 @@ class ImageFrame:
     def __init__(self, master, image):
         self.frame = ttk.Frame(master)
         self.canvas = tk.Canvas(self.frame)
-        self.xscroll = ttk.Scrollbar(self.frame, orient = tk.HORIZONTAL, command = self.canvas.xview)
-        self.yscroll = ttk.Scrollbar(self.frame, orient = tk.VERTICAL, command = self.canvas.yview)
-        self.canvas.grid(row=1, column= 0, sticky= 'nsew')
-        self.frame.columnconfigure(0, weight = 1)
-        self.frame.rowconfigure(1, weight = 1)
-        self.xscroll.grid(row = 2, column=0, sticky = 'ew')
-        self.yscroll.grid(row = 1, column=2, sticky = 'ns')
-        self.canvas.config(xscrollcommand = self.xscroll.set, yscrollcommand = self.yscroll.set)
-        self.setImage(image)
+        self.xscroll = ttk.Scrollbar(self.frame, orient=tk.HORIZONTAL, command=self.canvas.xview)
+        self.yscroll = ttk.Scrollbar(self.frame, orient=tk.VERTICAL, command=self.canvas.yview)
+        self.canvas.grid(row=1, column=0, sticky='nsew')
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(1, weight=1)
+        self.xscroll.grid(row=2, column=0, sticky='ew')
+        self.yscroll.grid(row=1, column=2, sticky='ns')
+        self.canvas.config(xscrollcommand=self.xscroll.set, yscrollcommand=self.yscroll.set)
+        self.set_image(image)
 
-    def setImage(self, image):
+    def set_image(self, image):
         self.image_file = image
         self.canvas.create_image(0, 0, image=self.image_file, anchor=tk.NW)
-        self.canvas.config(scrollregion = (0, 0, self.image_file.width(), self.image_file.height()))
+        self.canvas.config(scrollregion=(0, 0, self.image_file.width(), self.image_file.height()))
 
-    def getFrame(self):
+    def get_frame(self):
         return self.frame
 
 root = tk.Tk()
 image = tk.PhotoImage(file='../../data/textures/terrain.png')
 imageFrame = ImageFrame(root, image)
-imageFrame.getFrame().pack(expand=tk.YES, fill=tk.BOTH)
+imageFrame.get_frame().pack(expand=tk.YES, fill=tk.BOTH)
 
 root.mainloop()
