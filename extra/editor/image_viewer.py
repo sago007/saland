686533f640dcc969f23523035de0307d2e87d47a
Poul Sander <poul@poulsander.com>
2020-04-05 10:46:04 +0200

    Moved ImageFrame into a seperate unit

--- extra/editor/image_viewer.py
+++ extra/editor/image_viewer.py
@@ -2,29 +2,9 @@
 
 import tkinter as tk
 from tkinter import ttk
+from image_frame import *
 
 
-class ImageFrame:
-    def __init__(self, master, image):
-        self.frame = ttk.Frame(master)
-        self.canvas = tk.Canvas(self.frame)
-        self.xscroll = ttk.Scrollbar(self.frame, orient=tk.HORIZONTAL, command=self.canvas.xview)
-        self.yscroll = ttk.Scrollbar(self.frame, orient=tk.VERTICAL, command=self.canvas.yview)
-        self.canvas.grid(row=1, column=0, sticky='nsew')
-        self.frame.columnconfigure(0, weight=1)
-        self.frame.rowconfigure(1, weight=1)
-        self.xscroll.grid(row=2, column=0, sticky='ew')
-        self.yscroll.grid(row=1, column=2, sticky='ns')
-        self.canvas.config(xscrollcommand=self.xscroll.set, yscrollcommand=self.yscroll.set)
-        self.set_image(image)
-
-    def set_image(self, image):
-        self.image_file = image
-        self.canvas.create_image(0, 0, image=self.image_file, anchor=tk.NW)
-        self.canvas.config(scrollregion=(0, 0, self.image_file.width(), self.image_file.height()))
-
-    def get_frame(self):
-        return self.frame
 
 root = tk.Tk()
 image = tk.PhotoImage(file='../../data/textures/terrain.png')
