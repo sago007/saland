17871a610a721e51de3097f305a735dae613b435
Poul Sander <poul@poulsander.com>
2020-04-11 22:29:12 +0200

    Add scrollbars and extract treeview to seperate file

--- extra/editor/texture_selector.py
+++ extra/editor/texture_selector.py
@@ -3,6 +3,7 @@
 import tkinter as tk
 from tkinter import ttk
 from image_frame import *
+from tree_frame import *
 
 #This is the raltive directory to the base of the SagoSprite dir
 #I am launching from extra/editor so I need to go two levels up
@@ -19,13 +20,15 @@ def callback_select(event):
 
 
 root = tk.Tk()
-treeview = ttk.Treeview(root)
-treeview.grid(row=0, column=0, sticky='ns')
+treeFrame = TreeFrame(root)
+treeview = treeFrame.get_treeview()
+treeFrame.get_frame().grid(row=0, column=0, sticky='ns')
 for t in textures:
     treeview.insert('','end',t, text = t)
 root.columnconfigure(1, weight=1)
 root.rowconfigure(0, weight=1)
 treeview.bind('<<TreeviewSelect>>', callback_select)
+
 imageFrame = ImageFrame(root, None)
 imageFrame.get_frame().grid(row=0, column=1, sticky='nsew')
 
